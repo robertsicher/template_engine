3bda3ccf60671e090bc935cc0c0759515d99b9b0
robertsicher <62100753+robertsicher@users.noreply.github.com>
2020-12-01 17:57:17 +0000

    clean up

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-
 //Output
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "employees.html");
